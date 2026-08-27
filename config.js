e432d286662eac20f9050c12a58dc47efe924609
gigicirillo <gigicirillo@gmail.com>
2026-08-27 14:42:47 +0200

    Connect sales form to Google Sheets web app

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// Incolla qui l'URL della Web App ottenuta pubblicando google-apps-script/Code.gs.
-// Esempio: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
+// Google Apps Script Web App endpoint for Sales Daily.
 window.SALES_APP_CONFIG = {
-  GOOGLE_SCRIPT_URL: ""
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzSrMu96NeJENEcXlrsmcR4pDeLwxVmiwks0R7k1_H-vUh3M9TiQpe5ix8IjWAolyfzbQ/exec"
 };
