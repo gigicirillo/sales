32f7e6b5771974c17df10a7fc0409d9d2211eb89
gigicirillo <gigicirillo@gmail.com>
2026-08-27 14:29:26 +0200

    Add Google Sheets endpoint configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+// Incolla qui l'URL della Web App ottenuta pubblicando google-apps-script/Code.gs.
+// Esempio: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
+window.SALES_APP_CONFIG = {
+  GOOGLE_SCRIPT_URL: ""
+};
