cfc7169a654384158c014c205e2e10e1d7c3ca44
gigicirillo <gigicirillo@gmail.com>
2026-08-28 00:11:29 +0200

    Load pass voucher dynamic details module

--- config.js
+++ config.js
@@ -2,3 +2,7 @@
 window.SALES_APP_CONFIG = {
   GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzSrMu96NeJENEcXlrsmcR4pDeLwxVmiwks0R7k1_H-vUh3M9TiQpe5ix8IjWAolyfzbQ/exec"
 };
+
+const passVoucherScript=document.createElement('script');
+passVoucherScript.src='pass-vouchers.js?v=20260828-0010';
+document.head.appendChild(passVoucherScript);
