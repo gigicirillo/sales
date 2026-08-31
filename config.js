7aad5b1224ef82af074ebdb1dbd6f97bff55cd2b
gigicirillo <gigicirillo@gmail.com>
2026-08-31 19:23:13 +0200

    Load edit workflow for daily records

--- config.js
+++ config.js
@@ -6,3 +6,9 @@ window.SALES_APP_CONFIG = {
 const passVoucherScript=document.createElement('script');
 passVoucherScript.src='pass-vouchers.js?v=20260828-0010';
 document.head.appendChild(passVoucherScript);
+
+if(new URLSearchParams(location.search).has('editDate')){
+  const editEntryScript=document.createElement('script');
+  editEntryScript.src='edit-entry.js?v=20260831-1';
+  document.head.appendChild(editEntryScript);
+}
